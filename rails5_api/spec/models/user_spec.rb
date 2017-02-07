b60bd833bce05b6e2db0b2598a0c4e301aa603e7
hadwinzhy <hadwinzhy@gmail.com>
2017-02-07 17:36:05 +0800

    add rspec example

---
+++ rails5_api/spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe User, :type => :model do
+  it "orders by last name" do
+    #lindeman = User.create!(first_name: "Andy", last_name: "Lindeman")
+    #chelimsky = User.create!(first_name: "David", last_name: "Chelimsky")
+
+    expect(1).to eq(1)
+  end
+end
